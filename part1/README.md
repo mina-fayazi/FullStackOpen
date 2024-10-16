32da9c0febd3d66f98024f24e25193aa20f41927
Mina Fayazi <minafa64@gmail.com>
2024-10-17 01:42:04 +0330

    Add exercises 1.3-1.5 and update README

--- part1/README.md
+++ part1/README.md
@@ -10,3 +10,11 @@ This directory contains the exercises for Part 1 of the FullStackOpen course.
 ### 1.2: Course Information, step 2
 - Refactor the Content component to render three Part components of which each renders the name and number of exercises of one part.
 
+### 1.3: Course Information, step 3
+- Refactor the code to use objects for each part containing the name and the number of exercises.
+
+### 1.4: Course Information, step 4
+- Place the parts into an array of objects and pass them as props to the components.
+
+### 1.5: Course Information, step 5
+- Combine the course and parts into a single object and refactor the components accordingly.
--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -1,47 +1,54 @@
-const Header = ({ course }) => {
-  return <h1>{course}</h1>
-}
+const Header = ({ course }) => <h1>{course}</h1>
 
-const Part = ({ name, exercises }) => {
-  return (
-    <p>
-      {name} {exercises}
-    </p>
-  )
-}
+const Part = ({ name, exercises }) => (
+  <p>
+    {name} {exercises}
+  </p>
+)
 
-const Content = ({ part1, exercises1, part2, exercises2, part3, exercises3 }) => {
+const Content = ({ parts }) => {
   return (
     <div>
-      <Part name={part1} exercises={exercises1} />
-      <Part name={part2} exercises={exercises2} />
-      <Part name={part3} exercises={exercises3} />
+      {parts.map((part, index) => (
+        <Part key={index} name={part.name} exercises={part.exercises} />
+      ))}
     </div>
   )
 }
 
-const Total = ({ exercises1, exercises2, exercises3 }) => {
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+const Total = ({ parts }) => {
+  let totalExercises = 0
+  parts.forEach(part => {
+    totalExercises += part.exercises
+  })
+
+  return <p>Number of exercises {totalExercises}</p>
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
   return (
     <div>
-      <Header course={course} />
-      <Content
-        part1={part1} exercises1={exercises1}
-        part2={part2} exercises2={exercises2}
-        part3={part3} exercises3={exercises3}
-      />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }

b69385326cc001dbddfec3a28f72278116e2d7e8
Mina Fayazi <minafa64@gmail.com>
2026-06-21 02:40:33 +0330

    Minor fix

--- part8/fs-graphql/library-backend/db.js
+++ part8/fs-graphql/library-backend/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 
 const connectToDatabase = async (uri) => {
-  console.log('connecting to database URI:', uri)
-
   try {
     await mongoose.connect(uri)
     console.log('connected to MongoDB')
